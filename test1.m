cd79a79a73608705c86da902ffbc4147bc038ef6
Wuwan-Tong <wuwantong@yahoo.com>
2023-12-04 23:04:22 +0800

    Update test1.m

--- test1.m
+++ test1.m
@@ -1,5 +1,11 @@
-classdef test1
-   enumeration
-      Monday, Tuesday, Wednesday, Thursday, Friday
-   end
+w=8;
+h=8;
+temp_mask=zeros(h,w);
+for i=1:16
+    temp_mask(:,:)=masks_save(1,i,:,:);
+    figure
+    heatmap(temp_mask,'ColorLimits',[0 64])
+    tlt=['w=', num2str(w),', h=', num2str(h), ', wedge type=', num2str(i)];
+    title(tlt)
 end
+
