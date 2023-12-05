637c3f691d3f52583b30607263d87279e11296f6
Wuwan-Tong <wuwantong@yahoo.com>
2023-12-06 03:40:06 +0800

    up

--- generate_DCT_bases.m
+++ generate_DCT_bases.m
@@ -1,22 +1,29 @@
-block_size=[8 16 32];
+block_size_w=[8 16 32];
+block_size_h=[8 16 32];
 cvar=1-gray;
-for idx_size=1
-    b=block_size(idx_size);
-    D=dctmtx(b);
-    DCT_basis=zeros(b^2,b^2);
-
-    temp=kron(D,D);
-    for i=0:b^2-1
-        DCT_basis(floor(i/b)*b+1:floor(i/b)*b+b,mod(i,b)*b+1:mod(i,b)*b+b)=reshape(temp(i+1,:),[b b])';
-%         figure 
-%         set(gcf,'Position',[20,20,600,600]);
-%         img=heatmap(DCT_basis(floor(i/b)*b+1:floor(i/b)*b+b,mod(i,b)*b+1:mod(i,b)*b+b),'ColorMap',cvar,'ColorbarVisible','off','CellLabelColor','none');
-%         img.XDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
-%         img.YDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
-%         set(gcf,'windowstyle','normal')
-%         img.Position =[0 0 1 1];
-%         filename=['DCT_basis/b', num2str(b),'/b',num2str(b),'_y', num2str(floor(i/b)+1),'_x', num2str(mod(i,b)+1)];
-%         saveas(gcf,filename,'png')
+for iw=3
+    for ih=2
+        w=block_size_w(iw);
+        h=block_size_h(ih);
+        D1=dctmtx(w);
+        D2=dctmtx(h);
+        DCT_basis=zeros(h^2,w^2);
+        temp=kron(D1,D2);
+        for i=0:w*h-1
+            DCT_basis(mod(i,h)*h+1:mod(i,h)*h+h,floor(i/h)*w+1:floor(i/h)*w+w)=reshape(temp(i+1,:),[h w]);
+            figure 
+            set(gcf,'Position',[20,20,600,300]);
+            img=heatmap(DCT_basis(mod(i,h)*h+1:mod(i,h)*h+h,floor(i/h)*w+1:floor(i/h)*w+w),'ColorMap',cvar,'ColorbarVisible','off','CellLabelColor','none');
+            img.XDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
+            img.YDisplayLabels ={'','','','','','','','','','','','','','','',''};
+            set(gcf,'windowstyle','normal')
+            img.Position =[0 0 1 1];
+            filename=['DCT_basis/w', num2str(w),'_h', num2str(h),'/w',num2str(w),'_y', num2str(mod(i,h)+1),'_x', num2str(floor(i/h)+1)];
+            saveas(gcf,filename,'png')
+            close(gcf);
+        end
     end
 end
+figure
+heatmap(DCT_basis)
 
