6b5750e95bd1a9d39fd2af8149301fa5e2eb788c
Wuwan-Tong <wuwantong@yahoo.com>
2023-12-06 01:10:34 +0800

    up

--- generate_DCT_bases.m
+++ generate_DCT_bases.m
@@ -1,8 +1,22 @@
-D=dctmtx(8);
-DCT_basis=zeros(64,64);
-for i=0:7
-    for j=0:7
-        DCT_basis(i*8+1:i*8+8,j*8+1:j*8+8)=transpose(D(:,j+1)*D(i+1,:));
+block_size=[8 16 32];
+cvar=1-gray;
+for idx_size=1
+    b=block_size(idx_size);
+    D=dctmtx(b);
+    DCT_basis=zeros(b^2,b^2);
+
+    temp=kron(D,D);
+    for i=0:b^2-1
+        DCT_basis(floor(i/b)*b+1:floor(i/b)*b+b,mod(i,b)*b+1:mod(i,b)*b+b)=reshape(temp(i+1,:),[b b])';
+%         figure 
+%         set(gcf,'Position',[20,20,600,600]);
+%         img=heatmap(DCT_basis(floor(i/b)*b+1:floor(i/b)*b+b,mod(i,b)*b+1:mod(i,b)*b+b),'ColorMap',cvar,'ColorbarVisible','off','CellLabelColor','none');
+%         img.XDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
+%         img.YDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
+%         set(gcf,'windowstyle','normal')
+%         img.Position =[0 0 1 1];
+%         filename=['DCT_basis/b', num2str(b),'/b',num2str(b),'_y', num2str(floor(i/b)+1),'_x', num2str(mod(i,b)+1)];
+%         saveas(gcf,filename,'png')
     end
 end
-heatmap(DCT_basis)
+
--- test1.m
+++ test1.m
@@ -1,24 +1,24 @@
 w=8;
-h=16;
+h=32;
+load('C:\Users\Stacey\Desktop\GenerateMasksLFB\masks\sharp\masks_sharp_w8_h32.mat')
 temp_mask=zeros(h,w);
 cvar=1-gray;
 for i=1:16
     temp_mask(:,:)=masks_save(1,i,:,:);
     figure
-    set(gcf,'Position',[20,20,250,500]);
-    img=heatmap(temp_mask,'ColorLimits',[0 64],'ColorMap',cvar,'ColorbarVisible','off');
-    filename=['figures_png/smooth/w8_h16/smooth_w', num2str(w),'_h', num2str(h), '_wedge type', num2str(i)];
+    set(gcf,'Position',[20,20,150,600]);
+%     set(gcf,'Position',[20,20,150,600]);
+%     set(gcf,'Position',[20,20,250,500]);
+    img=heatmap(temp_mask,'ColorLimits',[0 64],'ColorMap',cvar,'ColorbarVisible','off','CellLabelColor','none');
+    filename=['figures_png/sharp/w8_h32/sharp_w', num2str(w),'_h', num2str(h), '_wedge type', num2str(i)];
     img.XDisplayLabels ={'','','','','','','',''};
-    img.YDisplayLabels ={'','','','','','','','','','','','','','','',''};
+    img.YDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
     set(gcf,'windowstyle','normal')
     img.Position =[0 0 1 1];
-%     img.Position = [0.1 0.25 0.78,1/2];
+%     img.FontSize = 15;
 %     img.YDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
-%     img.Position = [0.38 0 0.78/4,1];
 %     img.YDisplayLabels ={'','','','','','','','','','','','','','','',''};
-%     img.Position = [0.27 0 0.78/2,1];
 %     img.YDisplayLabels ={'','','','','','','',''};
-%     img.Position = [0.1 0 0.78,1];
     saveas(gcf,filename,'png')
 end
 
