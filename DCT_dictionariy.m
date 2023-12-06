c8e423346e19b32ff8bb005181ee14a9d519fbb1
Wuwan-Tong <wuwantong@yahoo.com>
2023-12-06 19:40:22 +0800

    up

--- DCT_dictionariy.m
+++ DCT_dictionariy.m
@@ -1,1 +1,35 @@
 %todo
+block_size_w=[8 16 32];
+block_size_h=[8 16 32];
+smooth='sharp/'; % 'smooth/'; or 'sharp/';
+ori=1;% if use original wedge, else =2 for complement
+cvar=1-gray;
+for iw=1%:1length(block_size_w)
+    for ih=1%:length(block_size_h)
+        % init
+        w=block_size_w(iw);
+        h=block_size_h(ih);
+        DCT_Dictionary=zeros(16,h,w,h,w);
+        wedge_current=zeros(h,w);
+        DCT_Dictionary_current=zeros(h,w);
+        filename_DCT=['DCT_basis/DCT_basis_w',num2str(w),'_h',num2str(h),'.mat'];
+        DCT_basis=cell2mat(struct2cell(load(filename_DCT)));
+%         filename_wedge=['masks/',smooth,'masks_w',num2str(w),'_h',num2str(h),'.mat']; % smooth
+        filename_wedge=['masks/',smooth,'masks_sharp_w',num2str(w),'_h',num2str(h),'.mat'];
+        wedge_all=cell2mat( struct2cell(load(filename_wedge)));
+        for wtype=1:16
+            for ipos_w=1:w
+                for ipos_h=1:h
+                    DCT_Dictionary_current(:,:)=DCT_basis((ipos_h-1)*h+1:ipos_h*h,(ipos_w-1)*w+1:ipos_w*w);
+                    wedge_current(:,:)=wedge_all(ori,wtype,:,:);
+                    DCT_Dictionary(wtype,ipos_h,ipos_w,:,:)=DCT_Dictionary_current.*wedge_current;
+                    heatmap(DCT_Dictionary_current.*wedge_current,'ColorMap',cvar)
+                end
+            end
+        end
+    end
+end
+                
+        
+        
+        
--- generate_DCT_bases.m
+++ generate_DCT_bases.m
@@ -1,7 +1,7 @@
 block_size_w=[8 16 32];
 block_size_h=[8 16 32];
 cvar=1-gray;
-for iw=3
+for iw=1
     for ih=2
         w=block_size_w(iw);
         h=block_size_h(ih);
@@ -11,16 +11,16 @@ for iw=3
         temp=kron(D1,D2);
         for i=0:w*h-1
             DCT_basis(mod(i,h)*h+1:mod(i,h)*h+h,floor(i/h)*w+1:floor(i/h)*w+w)=reshape(temp(i+1,:),[h w]);
-            figure 
-            set(gcf,'Position',[20,20,600,300]);
-            img=heatmap(DCT_basis(mod(i,h)*h+1:mod(i,h)*h+h,floor(i/h)*w+1:floor(i/h)*w+w),'ColorMap',cvar,'ColorbarVisible','off','CellLabelColor','none');
-            img.XDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
-            img.YDisplayLabels ={'','','','','','','','','','','','','','','',''};
-            set(gcf,'windowstyle','normal')
-            img.Position =[0 0 1 1];
-            filename=['DCT_basis/w', num2str(w),'_h', num2str(h),'/w',num2str(w),'_y', num2str(mod(i,h)+1),'_x', num2str(floor(i/h)+1)];
-            saveas(gcf,filename,'png')
-            close(gcf);
+%             figure 
+%             set(gcf,'Position',[20,20,600,300]);
+%             img=heatmap(DCT_basis(mod(i,h)*h+1:mod(i,h)*h+h,floor(i/h)*w+1:floor(i/h)*w+w),'ColorMap',cvar,'ColorbarVisible','off','CellLabelColor','none');
+%             img.XDisplayLabels ={'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''};
+%             img.YDisplayLabels ={'','','','','','','','','','','','','','','',''};
+%             set(gcf,'windowstyle','normal')
+%             img.Position =[0 0 1 1];
+%             filename=['DCT_basis/w', num2str(w),'_h', num2str(h),'/w',num2str(w),'_y', num2str(mod(i,h)+1),'_x', num2str(floor(i/h)+1)];
+%             saveas(gcf,filename,'png')
+%             close(gcf);
         end
     end
 end
